e92a83ed139131db7291eea6c771cd46957a6272
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2022-11-24 16:52:19 +0100

    OB_M3_T11_E1

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+class Estudiante {
+  constructor(nombre, asignaturas) {
+    this.nombre = nombre;
+    this.asignaturas = asignaturas;
+  }
+
+  obtenDatos() {
+    return {
+      nombre: this.nombre,
+      asignaturas: this.asignaturas,
+    };
+  }
+}
+
+let estudiante = new Estudiante("Daniel", ["Javascript", "HTML", "CSS"]);
+
+console.log(estudiante.obtenDatos());
